23d5f0b0ece09472e224ff2c3de4f4363a0cde79
赵旭 <33821934+zxuu@users.noreply.github.com>
2018-04-07 11:36:13 +0800

    Add files via upload

---
+++ 面向对象的六大原则/依赖倒置原则DIP.java
@@ -0,0 +1,44 @@
+public interface ImageCatch {
+
+    public Bitmap get(String url);
+
+    public void put(String url, Bitmap bmp);
+
+}
+
+public class MemmeryCatch implements ImageCatch {
+    @Override
+    public Bitmap get(String url) {
+        return null;
+    }
+
+    @Override
+    public void put(String url, Bitmap bmp) {
+        //...............................
+    }
+}
+
+public class DirCatch implements ImageCatch{
+    @Override
+    public Bitmap get(String url) {
+        return null;
+    }
+
+    @Override
+    public void put(String url, Bitmap bmp) {
+        //.................................
+    }
+}
+
+public class Test {
+
+    //“¿¿µ”⁄≥ÈœÛ£¨∂‡Ã¨£°
+    ImageCatch imageCatch = new MemmeryCatch();
+
+//    ImageCatch imageCatch = new DirCatch();
+
+    public void disPlay(){
+
+    }
+
+}
